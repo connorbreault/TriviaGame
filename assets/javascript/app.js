ebed6095fd008995d934d27d5815ec1c57823211
Spaghetti Jones <connor.breault@comcast.net>
2019-07-15 23:31:47 -0700

    pseudocode added

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//make a list of questions & answers
+//start button + function
+//randomly select and render questions to the container
+//start a timer
+//function to track user guesses
+//function to determine if user guess is correct
+//done button that stops timer and displays round stats
+//the timer = 0, hide questions and display round stats
+//restart button = start button
